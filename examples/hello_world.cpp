fe75e00da48b397bbebb50aa9e4c0950f2295694
Alexander Ptakhin <me@aptakhin.name>
2016-02-06 14:01:16 +0300

    Hello world example

---
+++ examples/hello_world.cpp
@@ -0,0 +1,22 @@
+#include <enji/http.h>
+
+using namespace enji;
+
+void index(const HttpRequest& req, HttpOutput& out) {
+    out.response();
+    out.add_headers({
+        { "Content-Type", "text/html; charset=utf-8" },
+    });
+    out.body("Hello, world!\n");
+}
+
+int main(int argc, char* argv[]) {
+    ServerOptions opts;
+    opts.port = 3001;
+    HttpServer server(std::move(opts));
+    server.routes({
+        { "/", index },
+    });
+    server.run();
+    return 0;
+}
